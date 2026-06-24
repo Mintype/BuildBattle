df8846b795a8e02d140b317fc0b59e5ac3a48516
Shahrian Masud <mintype.code@gmail.com>
2026-06-24 17:38:42 -0400

    Respawns at actual spawn

---
+++ src/main/java/org/mintype/buildBattle/BuildBattle.java
@@ -0,0 +1,36 @@
+package org.mintype.buildBattle;
+
+import org.bukkit.World;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuildBattle extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private Location getSpawn() {
+        World w = Bukkit.getWorlds().get(0);
+        return new Location(w, 0, -14, 0);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        e.getPlayer().teleport(getSpawn());
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e) {
+        Bukkit.getScheduler().runTask(this, () ->
+                e.getPlayer().teleport(getSpawn())
+        );
+    }
+}
